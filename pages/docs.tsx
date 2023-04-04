a980e2312dbae0a47cbb72a34707d7cf5125c4d3
luisfelipe2104 <100540998+luisfelipe2104@users.noreply.github.com>
2023-04-04 16:20:12 -0300

    starting the documentation

---
+++ pages/docs.tsx
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import { GetStaticProps, InferGetStaticPropsType, NextPage } from "next";
+import { useRouter } from "next/router";
+import SwaggerUI from "swagger-ui-react";
+import "swagger-ui-react/swagger-ui.css";
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { NODE_ENV } = process.env;
+
+  const isDev = NODE_ENV === "development";
+  return {
+    props: { isDev },
+  };
+};
+
+const Docs: NextPage = ({
+  isDev,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const router = useRouter();
+  useEffect(() => {
+    if (!isDev) router.push("/404");
+  }, [isDev]);
+
+  return <SwaggerUI url="/swagger.json" />;
+};
+
+export default Docs;
